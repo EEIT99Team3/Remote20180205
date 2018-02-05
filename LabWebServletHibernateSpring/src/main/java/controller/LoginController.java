86ab043dc4beac3214752a7ead3731840f182dfe
朱凱鴻 <lorenzochu666@gmail.com>
2018-02-05 11:58:01 +0800

    第三次Commit
    增加Controller的註解 // 這是一個git Lab test

--- LabWebServletHibernateSpring/src/main/java/controller/LoginController.java
+++ LabWebServletHibernateSpring/src/main/java/controller/LoginController.java
@@ -18,7 +18,7 @@ import model.CustomerBean;
 import model.CustomerService;
 
 @Controller
-public class LoginController {
+public class LoginController { // 這是一個git Lab test
 	@Autowired
 	private CustomerService customerService;
 	
